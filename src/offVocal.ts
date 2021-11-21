88bea42dabad14eff8d39c6d7fc31d0633b7d9cb
Masaki Kobayashi <contact@mkobayashi.me>
2021-11-22 00:17:05 +0900

    Impl `offVocal.ts`

---
+++ src/offVocal.ts
@@ -0,0 +1,113 @@
+import { readFile, writeFile } from "fs/promises"
+import path from "path"
+import { parse } from "csv-parse/sync"
+import { pipe } from "fp-ts/lib/function.js"
+import * as O from "fp-ts/lib/Option.js"
+import * as A from "fp-ts/lib/Array.js"
+import * as Map from "fp-ts/lib/Map.js"
+import * as string from "fp-ts/lib/string.js"
+
+import type { Line, Songs } from "../types/offVocal"
+
+const getLiens = async (): Promise<Line[]> => {
+  const csv = await (
+    await readFile(path.resolve("data", "offVocal.csv"))
+  ).toString()
+  const lines: string[][] = await parse(csv)
+
+  return lines
+    .slice(1)
+    .map(
+      ([
+        slug,
+        title,
+        titleOfOffVocal,
+        albumTitle,
+        publishedAt,
+        albumDescription,
+        albumURL,
+        remarks,
+      ]) => ({
+        slug,
+        title,
+        titleOfOffVocal,
+        albumTitle,
+        publishedAt,
+        albumDescription,
+        albumURL,
+        remarks,
+      }),
+    )
+}
+
+const groupBySong = (lines: Line[]): Songs => {
+  return pipe(
+    lines,
+    A.reduce<Line, Songs>(
+      new global.Map(),
+      (acc, { slug, title, titleOfOffVocal, ...album }) => {
+        const song = pipe(acc, Map.lookup(string.Eq)(slug))
+
+        return pipe(
+          acc,
+          Map.upsertAt(string.Eq)(slug, {
+            slug,
+            title,
+            titleOfOffVocal,
+
+            albums: O.isSome(song) ? [...song.value.albums, album] : [album],
+          }),
+        )
+      },
+    ),
+  )
+}
+
+const docgen = (songs: Songs): string => {
+  return (
+    "# ナナシス楽曲オフボーカル音源まとめ\n\n" +
+    pipe(
+      songs,
+      Map.toArray(string.Ord),
+      A.map(([_, { title, titleOfOffVocal, albums }]) => {
+        const albumsMd = albums
+          .map(
+            ({
+              albumTitle,
+              publishedAt,
+              albumDescription,
+              albumURL,
+              remarks,
+            }) =>
+              `
+- [${albumTitle}](${albumURL})
+
+  ${publishedAt}発売
+
+  ${remarks}
+            `.trim(),
+          )
+          .join("\n\n")
+
+        return `
+## ${title}
+
+\`${titleOfOffVocal}\`
+
+### 収録アルバム
+
+${albumsMd}
+    `.trim()
+      }),
+    ).join("\n\n")
+  )
+}
+
+//
+;(async () => {
+  const lines = await getLiens()
+  const songs = groupBySong(lines)
+  const doc = docgen(songs)
+
+  await writeFile(path.resolve("docs", "offVocal.md"), doc)
+})()
