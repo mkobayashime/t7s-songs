25d1c9108ea34955e637fbcfc6c82eb33b09080f
Masaki Kobayashi <contact@mkobayashi.me>
2021-11-24 00:55:02 +0900

    Add original songs data

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,13 @@
 export type SongSlug = string
+
+export type Song = Readonly<{
+  slug: SongSlug
+  /**
+   * t7s.jp 上で id として利用されている数字
+   */
+  officialId: number
+  title: string
+  publishedAt: string
+  description: string
+  image: string
+}>
