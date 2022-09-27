a010dba0af689858522e74a368a65c005f3e9a21
Masaki Kobayashi <contact@mkobayashi.me>
2022-09-28 05:05:21 +0900

    Add import assertions to JSON imports

--- data/offVocal/index.ts
+++ data/offVocal/index.ts
@@ -1,6 +1,6 @@
 import type { OffVocalItem } from "../../types/offVocal"
 
-import json from "./offVocal.json"
+import json from "./offVocal.json" assert { type: "json" }
 
 const offVocals: OffVocalItem[] = json.value
 
--- data/songs/index.ts
+++ data/songs/index.ts
@@ -1,6 +1,6 @@
 import type { Song, SongSlug } from "../../types"
 
-import json from "./songs.json"
+import json from "./songs.json" assert { type: "json" }
 
 export const songs: Song[] = json.value
 
